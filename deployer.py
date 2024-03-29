ef90ca7cd79cd5d01f9c29466a8069ad39557069
arithgrey <arithgrey@gmail.com>
2024-03-28 23:50:14 -0600

    [FEAT] is automatized the way to generated manifest files, on each resource defined on json config, is created service, deploy and secret if its defined

--- deployer.py
+++ deployer.py
@@ -1,6 +1,7 @@
 import os
 import yaml
-from dotenv import load_dotenv
+import json
+import base64
 
 class KubernetesResource:
     def __init__(self, name):
@@ -14,14 +15,13 @@ class Deployment(KubernetesResource):
         super().__init__(name)
         self.image = image
         self.replicas = replicas
+        self.metadata = {}
 
     def generate_yaml(self):
         deployment = {
             'apiVersion': 'apps/v1',
             'kind': 'Deployment',
-            'metadata': {
-                'name': f'microservice-{self.name}'
-            },
+            'metadata': self.metadata,
             'spec': {
                 'replicas': self.replicas,
                 'selector': {
@@ -50,33 +50,41 @@ class Deployment(KubernetesResource):
         return yaml.dump(deployment)
 
 class Service(KubernetesResource):
+    def __init__(self, name, port):
+        super().__init__(name)
+        self.metadata = {}
+        self.port = port
+
+
     def generate_yaml(self):
         service = {
             'apiVersion': 'v1',
             'kind': 'Service',
-            'metadata': {
-                'name': f'microservice-{self.name}'
-            },
+            'metadata': self.metadata,
             'spec': {
                 'selector': {
                     'app': f'microservice-{self.name}'
                 },
                 'ports': [{
                     'protocol': 'TCP',
-                    'port': 80,
-                    'targetPort': 8080
+                    'port': self.port,
+                    'targetPort': self.port
                 }]
             }
         }
         return yaml.dump(service)
 
 class Namespace(KubernetesResource):
+    def __init__(self, name):
+        super().__init__(name)
+        self.metadata = {}
+
     def generate_yaml(self):
         namespace = {
             'apiVersion': 'v1',
             'kind': 'Namespace',
             'metadata': {
-                'name': f'{self.name}-namespace'
+                'name': f'{self.name}'
             }
         }
         return yaml.dump(namespace)
@@ -85,66 +93,69 @@ class Secret(KubernetesResource):
     def __init__(self, name, data):
         super().__init__(name)
         self.data = data
+        self.metadata = {}
 
     def generate_yaml(self):
+        encoded_data = {key: base64.b64encode(value.encode('utf-8')).decode('utf-8') for key, value in self.data.items()}
         secret = {
             'apiVersion': 'v1',
             'kind': 'Secret',
             'metadata': {
-                'name': f'microservice-{self.name}-secret'
+                'name': f'microservice-{self.name}'
             },
-            'data': self.data
+            'data': encoded_data
         }
         return yaml.dump(secret)
 
 class KubernetesDeployer:
-    def __init__(self, microservice_name, docker_image, replicas):
-        self.microservice_name = microservice_name
-        self.docker_image = docker_image
-        self.replicas = replicas
-
-        # Cargar variables de entorno desde el archivo .env
-        load_dotenv()
-        self.load_environment_variables()
-
-    def load_environment_variables(self):
-        # Variables de entorno para PostgreSQL
-        self.db_name = os.getenv('DB_NAME')
-        self.db_user = os.getenv('DB_USER')
-        self.db_password = os.getenv('DB_PASSWORD')
-        self.db_host = os.getenv('DB_HOST')
-        self.db_port = os.getenv('DB_PORT')
+    def __init__(self, config):
+        self.config = config
+        self.microservice_name = config['MICROSERVICE_NAME']
+        self.secrets = config.get('SECRETS', {})
 
     def generate_resources(self):
-        postgresql_data = {
-            'DB_NAME': self.db_name.encode('utf-8'),
-            'DB_USER': self.db_user.encode('utf-8'),
-            'DB_PASSWORD': self.db_password.encode('utf-8'),
-            'DB_HOST': self.db_host.encode('utf-8'),
-            'DB_PORT': self.db_port.encode('utf-8')
-        }
+        namespace = Namespace(f'namespace-{self.microservice_name}')
 
-        resources = [
-            Deployment(f'{self.microservice_name}-deployment', self.docker_image, self.replicas),
-            Service(f'{self.microservice_name}-service'),
-            Namespace(f'{self.microservice_name}-namespace'),
-            Secret(f'{self.microservice_name}-secret', postgresql_data)
-        ]
+        resources = []
+        for resource_config in self.config['resources']:
+            component_name = resource_config['component_name']
+            docker_image = resource_config['docker_image']
+            replicas = resource_config['replicas']
+            port = resource_config.get('port')
 
-        return resources
+            deployment = Deployment(f'{component_name}-deployment-{self.microservice_name}', docker_image, replicas)            
+            resources.extend([deployment])
+
+            if port is not None:
+                service = Service(f'{component_name}-service-{self.microservice_name}', port)
+                resources.append(service)
+
+            if 'db_secrets' in resource_config:
+                secret_data = resource_config['db_secrets']
+                secret = Secret(f'{component_name}-secret-{self.microservice_name}', secret_data)
+                resources.append(secret)
+
+        # Asociar todos los recursos al mismo Namespace
+        for resource in resources:
+            resource.metadata['namespace'] = self.microservice_name
+
+        return [namespace] + resources
 
     def write_yaml_files(self, resources):
-        # Create 'k8s' directory if it doesn't exist
+        # Crear el directorio 'k8s' si no existe
         k8s_dir = 'k8s'
         if not os.path.exists(k8s_dir):
             os.makedirs(k8s_dir)
 
-        # Write YAML files in 'k8s' directory
+        # Escribir archivos YAML en el directorio 'k8s'
         for resource in resources:
-            with open(os.path.join(k8s_dir, f'{resource.name}.yaml'), 'w') as f:
+            with open(os.path.join(k8s_dir, f'{self.microservice_name}-{resource.name}.yaml'), 'w') as f:
                 f.write(resource.generate_yaml())
 
 if __name__ == "__main__":
-    deployer = KubernetesDeployer('references', 'my_docker_image:tag', 3)
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+
+    deployer = KubernetesDeployer(config)
     resources = deployer.generate_resources()
     deployer.write_yaml_files(resources)
